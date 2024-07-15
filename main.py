65973b0764c8c0bef380785c87d9882a56213f0c
mason <1606227397@qq.com>
2024-07-15 16:01:22 +0800

    调整fr参数值

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Quark:
         url = "https://drive-m.quark.cn/1/clouddrive/capacity/growth/info"
         querystring = {
             "pr": "ucpro",
-            "fr": "pc",
+            "fr": "android",
             "kps": self.param.get('kps'),
             "sign": self.param.get('sign'),
             "vcode": self.param.get('vcode')
